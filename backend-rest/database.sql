de398738a4dc6276c283b8e8e137a0ee8142f819
Narathip Hvanmarat <narathip@livingmobile.me>
2021-06-17 17:34:45 +0700

    create category table

--- backend-rest/database.sql
+++ backend-rest/database.sql
@@ -1,3 +1,11 @@
 CREATE DATABASE store_database;
 
 --\c store_database
+
+CREATE TABLE category(
+    category_id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    name VARCHAR(255) NOT NULL,
+    store_id uuid NOT NULL,
+    FOREIGN KEY(store_id) 
+	    REFERENCES store(store_id)
+);
