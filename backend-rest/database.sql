1d35aafeba0e1d1126df77cb45e3d883992872bb
Narathip Hvanmarat <narathip@livingmobile.me>
2021-06-17 17:25:36 +0700

    create store table

--- backend-rest/database.sql
+++ backend-rest/database.sql
@@ -1,3 +1,10 @@
 CREATE DATABASE store_database;
 
 --\c store_database
+
+CREATE TABLE store(
+    store_id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    name VARCHAR(255) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    rating INT NOT NULL
+);
