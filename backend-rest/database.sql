8fe0cec23eb1db1fbf127625af76db29ad53e577
Narathip Hvanmarat <narathip@livingmobile.me>
2021-06-17 17:45:27 +0700

    create menu table

--- backend-rest/database.sql
+++ backend-rest/database.sql
@@ -1,3 +1,12 @@
 CREATE DATABASE store_database;
 
 --\c store_database
+
+CREATE TABLE menu(
+    menu_id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    category_id uuid NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    price INT NOT NULL,
+    FOREIGN KEY(category_id) 
+	    REFERENCES category(category_id)
+);
